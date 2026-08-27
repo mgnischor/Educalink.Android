b008477f833c24cc07938adf5f52deae04073242
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 09:30:38 -0400

    atualização da versão do plugin Google Services para 4.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
   alias(libs.plugins.hilt) apply false
   alias(libs.plugins.kotlin.compose) apply false
 
-  id("com.google.gms.google-services") version "4.4.4" apply false
+  id("com.google.gms.google-services") version "4.5.0" apply false
 }
